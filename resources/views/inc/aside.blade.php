c7ccdfb1355ae7fd936ac24a7f02a58bf3e9c0f6
Алишер <hadajan@mail.ru>
2024-07-28 12:41:37 +0500

    2807
    Резерв

--- resources/views/inc/aside.blade.php
+++ resources/views/inc/aside.blade.php
@@ -1,56 +1,38 @@
 @section('aside')
-<style>
-    .nav-link:hover {
-        background-color: #e0e0e0; /* Цвет фона при наведении (не темно-серый) */
-        color: #000; /* Цвет текста при наведении */
-    }
-</style>
 <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 100%;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <svg class="bi pe-none me-2" width="40" height="32">
-            <use xlink:href="#bootstrap" />
-        </svg>
+        <i class="bi bi-bootstrap-fill pe-none me-2" style="font-size: 2rem;"></i>
         <span class="fs-4">Выберите</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
             <a href="{{ route('home') }}" class="nav-link active" aria-current="page">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#home" />
-                </svg>
+                <i class="bi bi-house-fill pe-none me-2"></i>
                 Главное
             </a>
         </li>
         <li>
             <a href="#" class="nav-link link-dark">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#speedometer2" />
-                </svg>
+                <i class="bi bi-speedometer2 pe-none me-2"></i>
                 Отчёт
             </a>
         </li>
         <li>
             <a href="#" class="nav-link link-dark">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#table" />
-                </svg>
+                <i class="bi bi-table pe-none me-2"></i>
                 Продукты
             </a>
         </li>
         <li>
             <a href="#" class="nav-link link-dark">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#grid" />
-                </svg>
+                <i class="bi bi-grid pe-none me-2"></i>
                 Покупатели
             </a>
         </li>
         <li>
             <a href="#" class="nav-link link-dark">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#people-circle" />
-                </svg>
+                <i class="bi bi-people-circle pe-none me-2"></i>
                 Заявка
             </a>
         </li>
--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,32 @@
 <?php
-namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Auth\SocialController;
 use App\Http\Controllers\SubscriptionController;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+// Статические страницы
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/prj', 'pricing')->name('price');
+Route::view('/blog', 'blog')->name('blog');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+// Контактные маршруты
+Route::prefix('contact')->group(function () {
+    Route::get('/all', [ContactController::class, 'allData'])->name('contact-data');
+    Route::post('/submit', [ContactController::class, 'submit'])->name('contact-form');
 
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
+    Route::get('/all/{id}', [ContactController::class, 'ShowOneMessage'])->name('contact-data-one');
+    Route::get('/alls/{id}/update', [ContactController::class, 'UpdateMessage'])->name('contact-update');
+    Route::post('/all/{id}/update', [ContactController::class, 'UpdateMessageSubmit'])->name('contact-update-submit');
+});
 
-Route::get('/prj', function () {
-    return view('pricing');
-})->name('price');
+// Маршруты авторизации через социальные сети
+Route::prefix('auth')->group(function () {
+    Route::get('{provider}', [SocialController::class, 'redirectToProvider']);
+    Route::get('{provider}/callback', [SocialController::class, 'handleProviderCallback']);
+});
 
-Route::get('/blog', function () {
-    return view('blog');
-})->name('blog');
-
-
-Route::get(
-    '/contact/all/{id}', [ContactController::class,
-    'ShowOneMessage']
-)->name('contact-data-one');
-
-Route::get(
-    '/contact/alls/{id}/update', [ContactController::class,
-    'UpdateMessage']
-)->name('contact-update');
-
-Route::post(
-    '/contact/all/{id}/update', [ContactController::class,
-    'UpdateMessageSubmit']
-)->name('contact-update-submit');
-
-Route::get('/contact/all', [ContactController::class, 'allData'])->name('contact-data');
-Route::post('/contact/submit', [ContactController::class, 'submit'])->name('contact-form');
-
-Route::get('auth/{provider}', [SocialController::class, 'redirectToProvider']);
-Route::get('auth/{provider}/callback', [SocialController::class, 'handleProviderCallback']);
+// Подписка
 Route::post('/subscribe', [SubscriptionController::class, 'subscribe'])->name('subscribe');
