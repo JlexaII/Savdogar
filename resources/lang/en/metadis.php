eee988a762500dec455fc0086b041f50a55a555e
Алишер <hadajan@mail.ru>
2024-08-15 10:58:16 +0500

    1
    1

--- resources/lang/en/metadis.php
+++ resources/lang/en/metadis.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'text' => 'Place free ads on AMS. Easily sell or find what you need among thousands of offers for real estate, transport, personal belongings and much more.',
-    'text2' => 'A new site for free ads has opened. Place your offers in various categories quickly and easily. Free placement and user-friendly interface.',
-    'text3' => 'Expanding the capabilities of the site for free ads. New categories for posting ads. Free posting and convenient search.',
+    'text' => 'USA New York Place free ads on AMS. Easily sell or find what you need among thousands of offers for real estate, transport, personal belongings and much more.',
+    'text2' => 'Tashkent A new site for free ads has opened. Place your offers in various categories quickly and easily. Free placement and user-friendly interface.',
+    'text3' => 'Moscov Expanding the capabilities of the site for free ads. New categories for posting ads. Free posting and convenient search.',
 ];
